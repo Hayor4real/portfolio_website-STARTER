969d8d9a13a6b042680e8b3b4749c13ff4e554cd
Akindele Ayorinde <AKINDELEAYORINDE55@GMAIL.COM>
2022-05-30 19:56:11 +0200

    added the hero page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <>
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Hi, I am <br />
+          Akindele Ayorinde Emmanuel
+        </SectionTitle>
+        <SectionText>
+          I am a Front-End Developer with demostrable skills on HTML, CSS,
+          Javascript,React.JS, Node.JS,Web API,Mongo DB Boostrap and CSS
+          framework. have been involved with various projects and other
+          impressive technological solutions
+        </SectionText>
+      </LeftSection>
+    </Section>
+  </>
 );
 
 export default Hero;
